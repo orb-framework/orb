de90653648a15b7e5170f453a917f8a316d5259a
Eric Hulser <ehulser@teslamotors.com>
2015-03-11 11:19:35 -0700

    added a transaction context to automatically close connections after finishing

--- orb/__init__.py
+++ orb/__init__.py
@@ -19,7 +19,7 @@ __email__ = 'eric.hulser@gmail.com'
 # define version information (major,minor,revision)
 __major__ = 4
 __minor__ = 4
-__revision__ = 3
+__revision__ = 4
 
 __version_info__ = (__major__, __minor__, __revision__)
 __version__ = '{0}.{1}.{2}'.format(*__version_info__)
@@ -38,6 +38,7 @@ from .data import *
 from .querying import *
 from .schema import *
 from .searching import *
+from .contexts import *
 
 #----------------------------------------------------------------------
 
---
+++ orb/contexts.py
@@ -0,0 +1,14 @@
+import orb
+
+
+class SingleTransaction(object):
+    """
+    This context will automatically close connections to a given set of databases.
+    If no databases are provided, then all databases in the system will be used.
+    """
+    def __init__(self, *databases):
+        self._databases = databases or orb.system.databases()
+
+    def __exit__(self, exc_type, error, traceback):
+        for db in self._databases:
+            db.disconnect()
