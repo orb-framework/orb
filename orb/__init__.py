96085dd4ac1a9a4f2eb8307b72babfa96341783a
Eric Hulser <ehulser@teslamotors.com>
2015-11-20 23:11:28 -0800

    using development tag for 2016 release

--- orb/__init__.py
+++ orb/__init__.py
@@ -21,7 +21,7 @@ except ImportError:
     __major__, __minor__, __revision__, __hash__ = (0, 0, 0, '')
 
 __version_info__ = (__major__, __minor__, __revision__)
-__version__ = '{0}.{1}.{2}'.format(*__version_info__)
+__version__ = '{0}.{1}.dev{2}'.format(*__version_info__)
 
 
 import logging
