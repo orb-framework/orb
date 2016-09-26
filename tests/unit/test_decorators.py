459eed187c4d45c51402c470afc482abc2c2eb51
Eric Hulser <ehulser@teslamotors.com>
2016-09-26 10:56:33 -0700

    * starting to add in unit tests

--- tests/unit/test_decorators.py
+++ tests/unit/test_decorators.py
@@ -1,2 +1,9 @@
 def test_virtual_decorator():
-    assert True
+    import orb
+    from orb.decorators import virtual
+
+    @virtual(orb.StringColumn)
+    def my_method():
+        pass
+
+    assert my_method.__orb__ is not None
