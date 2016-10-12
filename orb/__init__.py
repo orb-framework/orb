c8655bcbf2f98d55eaecea0fa9684629e9865cfc
Eric Hulser <ehulser@teslamotors.com>
2016-10-12 16:45:12 -0700

    * ensure the testing remains a development branch

--- orb/__init__.py
+++ orb/__init__.py
@@ -20,7 +20,7 @@ except ImportError:  # pragma: no cover
     __major__, __minor__, __revision__, __hash__ = (0, 0, 0, '')
 
 __version_info__ = (__major__, __minor__, __revision__)
-__version__ = '{0}.{1}.{2}'.format(*__version_info__)
+__version__ = '{0}.{1}.dev{2}'.format(*__version_info__)
 
 import demandimport
 import logging
